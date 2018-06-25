e68737f9ad3a9b95ef0c5d6c623575fc192f7660
Gyozmo <binantguillaume@gmail.com>
2018-06-25 13:16:06 +0200

    DB created + added content in storage and client

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ var mysql = require('mysql');
 
 app.set('view engine', 'ejs');
 
+        //SQL
+//connection parameters
+var con = mysql.createConnection({
+    host: "sql7.freemysqlhosting.net",
+    user: "sql7244511",
+    password: "wcUkbMcLNl"
+});
+
+//connection
+con.connect(function(err){
+    if (err) throw err;
+    console.log("connected!")
+})
+
+//create DB
+con.query("CREATE DATABASE mediateque", function(err, result){
+    if(err) throw err;
+    console.log("mediateque created");
+})
 
 
+//EXPRESS
 app.get('/', function(req,res){
     res.render('index')
 })
