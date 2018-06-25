ab53660a044da4715290f819df9415c6e739846a
Gyozmo <binantguillaume@gmail.com>
2018-06-25 10:58:11 +0200

    Route index + bootstrap + navbar

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var sql = require('mysql');
+
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', function(req,res){
+    res.render('index')
+})
+
+app.listen(3000, function(){
+    console.log('listening on 3000');
+})
+
+
+
+/*  SERVER SQL
+Server: sql7.freemysqlhosting.net
+Name: sql7244511
+Username: sql7244511
+Password: wcUkbMcLNl
+Port number: 3306
+*/
